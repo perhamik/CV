df952f33e5d1cc14e0a481a2cbcfbcd43217881c
Denys Yaroshenko <jaroshenkodenis97@gmail.com>
2022-06-08 19:27:18 +0300

    rm unused

--- next.config.js
+++ next.config.js
@@ -1,43 +1,5 @@
 const withPlugins = require('next-compose-plugins')
 
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self';
-  child-src example.com;
-  style-src 'self' example.com;
-  font-src 'self';  
-`
-
-const securityHeaders = [
-  // { key: 'Cache-Control', value: 'Cache-Control: no-cache, no-store, max-age=0, must-revalidate' },
-  { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
-  { key: 'X-DNS-Prefetch-Control', value: 'on' },
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubDomains; preload',
-  },
-  {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block',
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'SAMEORIGIN',
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin',
-  },
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
-  },
-]
-
 module.exports = withPlugins([], {
   async headers() {
     return [
